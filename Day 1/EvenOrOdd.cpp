dc0fceb112e9ec8abacccbae67092008384c6f6f
Vansh Chaudhary <vanshchoudhary663@gmail.com>
2026-08-31 18:39:09 +0530

    Question 3 done

---
+++ Day 1/EvenOrOdd.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cout<<"N=";
+    cin>>N;
+    if(N%2==0)cout<<"Even";
+    else{
+      cout<<"Odd";
+    }
+    return 0;
+}
