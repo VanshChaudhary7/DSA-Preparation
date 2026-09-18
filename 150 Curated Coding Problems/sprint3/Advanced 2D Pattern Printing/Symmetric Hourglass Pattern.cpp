2658f6be0d5c86369ed88fc248a1411dbb64a539
Vansh Chaudhary <vanshchoudhary663@gmail.com>
2026-09-18 15:06:53 +0530

    HourGlassPattern done

--- 150 Curated Coding Problems/sprint3/Advanced 2D Pattern Printing/Symmetric Hourglass Pattern.cpp
+++ 150 Curated Coding Problems/sprint3/Advanced 2D Pattern Printing/Symmetric Hourglass Pattern.cpp
@@ -12,26 +12,26 @@ int main() {
     //  ****
     // ******
     int n;
+    cout<<"N=";
     cin >> n;
 
-  for(int i=1;i<=n;i++)
-  {
-    for(int spaces=1;spaces<=2*i;i++){
-      cout<<" ";
+    for (int i = 1; i <= n; i++) {
+        for (int spaces = 1; spaces <= i; spaces++) {
+            cout << " ";
+        }
+        for (int k = 1; k <= 2 * (n - i) + 1; k++) {
+            cout << "*";
+        }
+        cout << endl;
     }
-    for(int k=1;k<=2*(n-i)+1;k++){
-      cout<<"*";
+    for (int i = n - 1; i >= 1; i--) {
+        for (int spaces = 1; spaces <= i; spaces++) {
+            cout << " ";
+        }
+        for (int k = 1; k <= 2 * (n - i) + 1; k++) {
+            cout << "*";
+        }
+        cout << endl;
     }
-    cout<<endl;
-  }
-  for (int i = n; i >=1; i--) {
-      for (int spaces = 1; spaces <= 2 * i; i++) {
-          cout << " ";
-      }
-      for (int k = 1; k <= 2 * (n - i) + 1; k++) {
-          cout << "*";
-      }
-      cout << endl;
-  }
     return 0;
 }
